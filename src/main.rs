35ac1d26f9bdb8cf9741740f2130687008c17fe0
Amon Benson <amonkbenson@gmail.com>
2026-01-13 10:44:41 +0100

    more midi class updates

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,14 @@ use embassy_stm32::gpio::{AnyPin, Level, Output, Speed};
 use embassy_stm32::usb::Driver;
 use embassy_stm32::{Config, bind_interrupts, peripherals, usb};
 use embassy_stm32::adc::{Adc, AdcChannel, AdcConfig, AnyAdcChannel, Resolution, SampleTime};
+use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
+use embassy_sync::channel::Channel;
+use embassy_usb::class::midi::MidiClass;
+use embassy_usb::driver::EndpointError;
 use embassy_usb::{Builder, Handler};
-use embassy_usb::class::hid::{HidReaderWriter, ReportId, RequestHandler, State};
 use embassy_usb::control::OutResponse;
+use midi_types::MidiMessage;
+use midi_types::status::CONTROL_CHANGE;
 use usbd_hid::descriptor::{KeyboardReport, SerializedDescriptor};
 use crate::channel_strip::ChannelStrip;
 
@@ -57,132 +62,90 @@ async fn main(_spawner: Spawner) {
     );
 
     let mut config = embassy_usb::Config::new(0x1209, 0xd2b3);
-    config.manufacturer = Some("Amon Benson");
+    config.manufacturer = Some("schlegelflegel");
     config.product = Some("Midi Expressor");
-    config.serial_number = Some("1337");
+    config.serial_number = Some("0.3.0");
     config.max_power = 100;
     config.max_packet_size_0 = 64;
 
+    // Required for windows compatibility.
+    // https://developer.nordicsemi.com/nRF_Connect_SDK/doc/1.9.1/kconfig/CONFIG_CDC_ACM_IAD.html#help
+    config.device_class = 0xEF;
+    config.device_sub_class = 0x02;
+    config.device_protocol = 0x01;
+    config.composite_with_iads = true;
+
+    // Create embassy-usb DeviceBuilder using the driver and config.
+    // It needs some buffers for building the descriptors.
     let mut config_descriptor = [0; 256];
     let mut bos_descriptor = [0; 256];
-    // Microsoft OS descriptor??
-    let mut msos_descriptor = [0; 256];
     let mut control_buf = [0; 64];
 
-    let mut request_handler = MyRequestHandler {};
-    let mut device_handler = MyDeviceHandler::new();
-
-    let mut channel_strips = [ChannelStrip::default(); NUM_CHANNELS];
-
-    let mut state = State::new();
     let mut builder = Builder::new(
         driver,
         config,
         &mut config_descriptor,
         &mut bos_descriptor,
-        &mut msos_descriptor,
+        &mut [], // no msos descriptors
         &mut control_buf,
     );
-
-    builder.handler(&mut device_handler);
-
-    // Create classes on the builder.
-    let config = embassy_usb::class::hid::Config {
-        report_descriptor: KeyboardReport::desc(),
-        request_handler: None,
-        poll_ms: 60,
-        max_packet_size: 8,
-    };
-
-    let hid = HidReaderWriter::<_, 1, 8>::new(&mut builder, &mut state, config);
-
+    let mut midi_class = MidiClass::new(&mut builder, 1, 1, 64);
     let mut usb = builder.build();
-    let usb_fut = usb.run();
-    let (reader, mut writer) = hid.split();
 
-    let in_fut = async {
+    let mut channel_strips = [ChannelStrip::default(); NUM_CHANNELS];
+
+    let mut usb_fut = usb.run();
+
+    let mut midi_fut = async {
         loop {
-            for (i, channel_strip) in channel_strips.iter_mut().enumerate() {
-                let raw_value = adc.blocking_read(&mut adc_channels[i], SampleTime::CYCLES24_5);
+            midi_class.wait_connection().await;
+            info!("USB Connected");
+            let _ = midi_session(&mut midi_class).await;
+            info!("USB Disconnected");
+        }
+    };
 
-                channel_strip.process(raw_value);
+    // let in_fut = async {
+    //     loop {
+    //         for (i, channel_strip) in channel_strips.iter_mut().enumerate() {
+    //             let raw_value = adc.blocking_read(&mut adc_channels[i], SampleTime::CYCLES24_5);
 
-                if channel_strip.changed() {
-                    info!("Channel {}: Value = {}", i, channel_strip.value())
-                }
+    //             channel_strip.process(raw_value);
+
+    //             if channel_strip.changed() {
+    //                 info!("Channel {}: Value = {}", i, channel_strip.value())
+    //             }
+    //         }
+    //     }
+    // };
+
+    join(usb_fut, midi_fut).await;
+}
+
+pub struct Disconnected;
+
+impl From<EndpointError> for Disconnected {
+    fn from(value: EndpointError) -> Self {
+        match value {
+            EndpointError::BufferOverflow => defmt::panic!("Buffer overflow"),
+            EndpointError::Disabled => Disconnected,
+        }
+    }
+}
+
+static MIDI_QUEUE: Channel<ThreadModeRawMutex, MidiMessage, 10> = Channel::new();
+
+pub async fn midi_session<'d, T: usb::Instance + 'd>(midi: &mut MidiClass<'d, Driver<'d, T>>) -> Result<(), Disconnected> {
+    loop {
+        let msg = MIDI_QUEUE.receive().await;
+        match msg {
+            MidiMessage::ControlChange(channel, control, value) => {
+                let cin = CONTROL_CHANGE >> 4;
+                let status = CONTROL_CHANGE << 4 | u8::from(channel);
+                let packet = [CONTROL_CHANGE >> 4, status, u8::from(control), u8::from(value)];
+                midi.write_packet(&packet).await?;
             }
-        }
-    };
-
-    let out_fut = async {
-        reader.run(false, &mut request_handler).await;
-    };
-
-    join(usb_fut, join(in_fut, out_fut)).await;
-}
-
-struct MyRequestHandler {}
-
-impl RequestHandler for MyRequestHandler {
-    fn get_report(&mut self, id: ReportId, buf: &mut [u8]) -> Option<usize> {
-        info!("Get report for {:?}", id);
-        None
-    }
-
-    fn set_report(&mut self, id: ReportId, data: &[u8]) -> OutResponse {
-        info!("Set report for {:?}: {=[u8]}", id, data);
-        OutResponse::Accepted
-    }
-
-    fn get_idle_ms(&mut self, id: Option<ReportId>) -> Option<u32> {
-        info!("Get idle rate for {:?}", id);
-        None
-    }
-
-    fn set_idle_ms(&mut self, id: Option<ReportId>, duration_ms: u32) {
-        info!("Set idle rate for {:?} to {:?}", id, duration_ms)
-    }
-}
-
-struct MyDeviceHandler {
-    configured: AtomicBool,
-}
-
-impl MyDeviceHandler {
-    fn new() -> Self {
-        MyDeviceHandler {
-            configured: AtomicBool::new(false),
-        }
-    }
-}
-
-impl Handler for MyDeviceHandler {
-    fn enabled(&mut self, enabled: bool) {
-        self.configured.store(false, Ordering::Relaxed);
-
-        if enabled {
-            info!("Device enabled");
-        } else {
-            info!("Device disabled");
-        }
-    }
-
-    fn reset(&mut self) {
-        self.configured.store(false, Ordering::Relaxed);
-        info!("Bus reset, the Vbus current limit is 100mA");
-    }
-
-    fn addressed(&mut self, addr: u8) {
-        self.configured.store(false, Ordering::Relaxed);
-        info!("USB address set to: {}", addr);
-    }
-
-    fn configured(&mut self, configured: bool) {
-        self.configured.store(configured, Ordering::Relaxed);
-        if configured {
-            info!("Device configured, it may now draw up to the configured current limit from Vbus.")
-        } else {
-            info!("Device is no longer configured, the Vbus current limit is 100mA.");
+            _ => {}
         }
     }
 }
